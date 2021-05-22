5e0ec6706596785513f576f11c083a40cfed5824
Marc Cornet <marccornet@gmail.com>
2021-05-22 09:13:15 +0200

    database fix

--- app/config.py
+++ app/config.py
@@ -8,6 +8,6 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
     # Database settings
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1) or \
     'sqlite:///' + os.path.join(basedir, database_name)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
